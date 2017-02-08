bbfe1fff5a37612b9b35a7c303e14ba752079b50
giuseppeurso-eu <urso.giu@gmail.com>
2017-02-08 15:44:26 +0100

    Sharing project Camel FTP Consumer

---
+++ camel-ftp-consumer/src/test/java/eu/giuseppeurso/activemq/camelftpconsumer/CamelExecutorTest.java
@@ -0,0 +1,44 @@
+package eu.giuseppeurso.activemq.camelftpconsumer;
+
+import eu.giuseppeurso.activemq.camelftpconsumer.CamelExecutor;
+import junit.framework.TestCase;
+
+/**
+ * Test cases 
+ * 
+ * @author Giuseppe Urso
+ * 
+ */
+public class CamelExecutorTest extends TestCase {
+
+	private static String testResourceDir;
+	private static String testFile01;
+
+	@Override
+	protected void setUp() throws Exception {
+		testResourceDir = "src/test/resources";	
+		testFile01 = testResourceDir + "/test.txt";
+	}
+
+	@Override
+	protected void tearDown() throws Exception {
+		super.tearDown();
+	}
+		
+	/**
+	 * Test 
+	 * 
+	 */
+	public void testInit() {
+		boolean assertActual = false;
+		CamelExecutor camelExecutor = new CamelExecutor();
+		try {
+			camelExecutor.init();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}finally {
+			assertActual=true;
+			assertEquals("TEST FAILURE " + this.getName(), true, assertActual);	
+		}
+	}
+}
